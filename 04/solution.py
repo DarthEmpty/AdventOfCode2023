af4bbbf6b6f1162df1d8ceb5be66f2c3ec50240b
Joel Clarke <joel.clarke273@gmail.com>
2023-12-05 16:44:37 +0000

    Add get_win_quantity function (day 4)

--- 04/solution.py
+++ 04/solution.py
@@ -1,31 +1,28 @@
 from typing import List
-from sortedcontainers import SortedList
 import re
 
 FILENAME = "04/input.txt"
 
 
-def part_1(contents: List[str]) -> int:
-    total_points = 0
-    
-    for card in contents:
-        winning_nos = set(card[:10])
-        possessed_nos = set(card[10:])
-        if (power := len(winning_nos.intersection(possessed_nos))) > 0:
-            total_points += 2**(power-1)
-    
-    return total_points
+def get_win_quantity(card: List[str]) -> int:
+    return len(set(card[:10]).intersection(set(card[10:])))
 
 
-def part_2(contents: List[str]) -> int:
+def part_1(contents: List[List[str]]) -> int:
+    return sum(
+        2**(power-1)
+        for card in contents
+        if (power := get_win_quantity(card)) > 0
+    )
+
+
+def part_2(contents: List[List[str]]) -> int:
     # card_collection is a mapping between the card ID and the quantity of said card.
     # i.e. At first, we have 1 copy of all cards
     card_collection = [1 for _ in range(len(contents))]
     
     for i, card in enumerate(contents):
-        winning_nos = set(card[:10])
-        possessed_nos = set(card[10:])
-        quantity_won = len(winning_nos.intersection(possessed_nos))
+        quantity_won = get_win_quantity(card)
         
         # Identical cards will always produce the same result
         # so we don't have to evaluate them individually.

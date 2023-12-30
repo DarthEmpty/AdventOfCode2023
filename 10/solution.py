f1512f7ffd99e478537ee6f7333f5d6f216739d5
Joel Clarke <joel.clarke273@gmail.com>
2023-12-30 21:06:14 +0000

    Complete day 10

--- 10/solution.py
+++ 10/solution.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from maze import Maze
+from maze import Maze, CARDINALS
 
 FILENAME = "10/input.txt"
 
@@ -20,13 +20,37 @@ def part_1(maze: Maze) -> int:
         
         steps, current = to_visit.pop(0)
     
-    print(maze)
-    
     return steps
 
 
-def part_2(maze: np.ndarray) -> int:
-    return 0
+def part_2(maze: Maze) -> int:
+    inside = False
+    pipes_seen = []
+    pipes_enclosed = 0
+    
+    for coord, pipe in np.ndenumerate(maze._grid):        
+        if inside and not maze.has_been_visited(coord):
+            pipes_enclosed += 1
+        
+        elif maze.has_been_visited(coord):
+            pipes_seen.extend(Maze.PIPE_DIRECTIONS[pipe])
+            
+            norths = pipes_seen.count(CARDINALS["north"])
+            souths = pipes_seen.count(CARDINALS["south"])
+            
+            # Crossing 2 pipes that both point north or both point south
+            # means that we've towed the border of the loop
+            # and haven't entered it
+            if norths > 1 or souths > 1:
+                pipes_seen.clear()
+            
+            # If we cross a north-pointing pipe and a south pointing pipe
+            # in the same row, we have crossed the border of the loop.
+            elif norths == 1 and souths == 1:
+                inside = not inside
+                pipes_seen.clear()
+
+    return pipes_enclosed
 
 
 if __name__ == "__main__":    
@@ -36,4 +60,5 @@ if __name__ == "__main__":
     maze = Maze([[char for char in line] for line in contents])
     print(part_1(maze))
     print(part_2(maze))
+    print(maze)
 
